95abb5a851ab016f816134184ac8d04242838cc3
jsweetpotato37 <jsweetpotato37@gmail.com>
2023-12-16 18:48:44 +0900

    jisu change

--- mission02/client/js/main.js
+++ mission02/client/js/main.js
@@ -8,7 +8,10 @@
 
 */
 
+console.log(data);
+
 const nav = document.querySelector(".nav");
+const list = nav.querySelectorAll("li");
 const visualImage = document.querySelector(".visual img");
 const Letter = document.querySelector(".nickName");
 
@@ -18,25 +21,25 @@ function handleClick(e) {
   let li = target.closest("li");
 
   if (!li) return;
-
   let index = li.dataset.index;
-  console.log(data);
-  const list = Array.from(nav.children);
 
   //li를 전부 돌아가면서 리무브해버림
   list.forEach((li) => {
     li.classList.remove("is-active");
   });
 
-  // const button = e.target.closest("button");
+  const button = e.target.closest("button");
   // visualImage.src = a.href;//이런 방법도 있지만 alt설명이 전혀 바뀌지 않는다.
 
   //내가 n번째 li를 선택하고 있는가?
   //data-index 수집하기
 
-  // visualImage.src = `./assets/part01/visual${index}.jpg`;
-  // visualImage.src = `./assets/${index1}.jpeg`;
-  // visualImage.alt = data[index - 1].alt;
+  // data에서 특정 프로퍼티만 가져오는 방법
+  // 그러면 data에 있는 프로퍼티를 어떻게 사용해야
+  // visualImage의 alt와 src를 바꾸고
+  // body의 background 스타일을 변경할 수 있을까?
+
+  visualImage.alt = data[index - 1].alt;
 
   li.classList.add("is-active");
 }
