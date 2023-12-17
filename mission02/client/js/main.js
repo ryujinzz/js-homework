517eb6134051a62d7431e7460e53078dd25b9efd
ryujinzz <kdo2240k@naver.com>
2023-12-17 16:57:07 +0900

    오디오 설정 완료

--- mission02/client/js/main.js
+++ mission02/client/js/main.js
@@ -7,9 +7,6 @@
 5. 함수 분리
 
 */
-
-console.log(data);
-
 const nav = document.querySelector(".nav");
 const list = nav.querySelectorAll("li");
 const visualImage = document.querySelector(".visual img");
@@ -23,25 +20,28 @@ function handleClick(e) {
   if (!li) return;
   let index = li.dataset.index;
 
-  //li를 전부 돌아가면서 리무브해버림
   list.forEach((li) => {
     li.classList.remove("is-active");
   });
-
-  const button = e.target.closest("button");
-  // visualImage.src = a.href;//이런 방법도 있지만 alt설명이 전혀 바뀌지 않는다.
-
-  //내가 n번째 li를 선택하고 있는가?
-  //data-index 수집하기
-
-  // data에서 특정 프로퍼티만 가져오는 방법
-  // 그러면 data에 있는 프로퍼티를 어떻게 사용해야
-  // visualImage의 alt와 src를 바꾸고
-  // body의 background 스타일을 변경할 수 있을까?
-
-  visualImage.alt = data[index - 1].alt;
-
   li.classList.add("is-active");
+
+  //이미지와 글자 변경
+  visualImage.alt = data[index - 1].alt;
+  visualImage.src = `./assets/${data[index - 1].name.toLowerCase()}.jpeg`;
+  Letter.textContent = data[index - 1].name;
+
+  //배경색 변경
+  const color1 = data[index - 1].color[0];
+  const color2 = data[index - 1].color[1];
+  document.body.style.backgroundImage = `linear-gradient(to bottom right, ${color1}, ${color2})`;
+
+  //오디오 재생 추가
+  let audios = new Audio(
+    `./assets/audio/${data[index - 1].name.toLowerCase()}.m4a`
+  );
+  audios.loop = false; // 반복재생하지 않음
+  audios.volume = 0.3; // 음량 설정
+  audios.play(); // sound1.mp3 재생
 }
 
 nav.addEventListener("click", handleClick);
