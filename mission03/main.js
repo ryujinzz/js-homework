d7984ee6829d84bbb507b87eee0c713770a6c15e
ryujinzz <kdo2240k@naver.com>
2023-12-25 23:45:55 +0900

    롤링페이퍼 기본 셋팅

--- mission03/main.js
+++ mission03/main.js
@@ -1,15 +1,102 @@
-import Swiper from "swiper";
-import "swiper/css";
+/* global gsap*/
+import {
+  insertLast,
+  xhrPromise,
+  tiger,
+  getNode as $,
+  renderUserCard,
+  changeColor,
+  renderSpinner,
+  delayP,
+  renderEmptyCard,
+  clearContents,
+} from "./lib/index.js";
 
-const swiper = new Swiper(".swiper-container", {
-  loop: true,
-  centeredSlides: true,
-  slidesPerView: "auto",
-  slideToClickedSlide: true,
-  effect: "coverflow",
-  coverflowEffect: {
-    rotate: 0,
-    slideShadows: false,
-    stretch: 70,
-  },
-});
+const END_POINT = "http://localhost:3000/users";
+//phase - 1
+
+const userCardInner = $(".user-card-inner");
+//1. user 데이터를 fetch하기
+//2. 함수 안에 넣기
+async function renderUserList() {
+  renderSpinner(userCardInner);
+  try {
+    await delayP(1000);
+    gsap.to(".loadingSpinner", {
+      opacity: 0,
+      onComplete() {
+        $(".loadingSpinner").remove();
+      },
+    });
+    const response = await tiger.get(END_POINT);
+    const userData = response.data;
+    userData.forEach(
+      (data) => renderUserCard(userCardInner, data)
+      //3. 유저 테이터 (이름만) 화면에 렌더링
+    );
+    changeColor(".user-card");
+    gsap.from(".user-card", {
+      x: 100,
+      opacity: 0,
+      stagger: {
+        from: "edge",
+        each: 0.1,
+      },
+    });
+  } catch (err) {
+    renderEmptyCard(userCardInner);
+  }
+}
+
+renderUserList();
+
+function handleDelete(e) {
+  //이벤트 위임
+  const button = e.target.closest("button");
+  //버튼만 수집
+  const article = e.target.closest("article");
+  //아티클만 수집
+  if (!article || !button) return;
+  const id = article.dataset.index.slice(5);
+  tiger.delete(`${END_POINT}/${id}`).then(() => {
+    clearContents(userCardInner);
+    renderUserList();
+  });
+}
+
+userCardInner.addEventListener("click", handleDelete);
+
+const createButton = $(".create");
+const cancelButton = $(".cancel");
+const doneButton = $(".done");
+
+function handleCreate() {
+  gsap.to(".pop", { autoAlpha: 1 });
+}
+
+function handleCancel(e) {
+  e.stopPropagation();
+  gsap.to(".pop", { autoAlpha: 0 });
+}
+
+function handleDone(e) {
+  e.preventDefault();
+
+  const name = $("#nameField").value;
+  const email = $("#emailField").value;
+
+  const obj = {
+    name,
+    email,
+  };
+
+  tiger.post(END_POINT, obj).then(() => {
+    clearContents(userCardInner);
+    renderUserList();
+    gsap.to(".pop", { autoAlpha: 0 });
+  });
+}
+
+createButton.addEventListener("click", handleCreate);
+cancelButton.addEventListener("click", handleCancel);
+doneButton.addEventListener("click", handleDone);
